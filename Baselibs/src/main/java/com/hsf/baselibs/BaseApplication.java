914941b5f6569b7cc65df0ffe872007f621ef319
v_vshfhuang <1152594956@qq.com>
2023-02-16 17:32:55 +0800

    改造成多层次复用

--- Baselibs/src/main/java/com/hsf/baselibs/BaseApplication.java
+++ Baselibs/src/main/java/com/hsf/baselibs/BaseApplication.java
@@ -4,6 +4,8 @@ import android.app.Application;
 import android.content.Context;
 import android.util.Log;
 
+import com.alibaba.android.arouter.launcher.ARouter;
+
 public class BaseApplication extends Application {
 
     private static Context context;
@@ -14,9 +16,20 @@ public class BaseApplication extends Application {
         Log.d("Daisy", "BaseApplication 启动");
 
         context = getApplicationContext();
+
+        if (isDebug()) {
+            ARouter.openLog();
+            ARouter.openDebug();
+        }
+
+        ARouter.init(this);
     }
 
     public static Context getTheContext() {
         return context;
+    }
+
+    private boolean isDebug() {
+        return BuildConfig.DEBUG;
     }
 }
