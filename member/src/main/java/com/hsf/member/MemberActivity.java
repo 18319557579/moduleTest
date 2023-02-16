0c1472678e9846d7db9cae5e8fe85e0272e6e7f5
v_vshfhuang <1152594956@qq.com>
2023-02-16 14:10:17 +0800

    增加Member跳转Login的操作

--- member/src/main/java/com/hsf/member/MemberActivity.java
+++ member/src/main/java/com/hsf/member/MemberActivity.java
@@ -3,8 +3,10 @@ package com.hsf.member;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
+import com.alibaba.android.arouter.launcher.ARouter;
 import com.hsf.baselibs.BaseActivity;
 
 @Route(path = "/member/member")
@@ -14,5 +16,12 @@ public class MemberActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 //        setContentView(R.layout.activity_member);
+
+        setClickSkip1(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ARouter.getInstance().build("/login/login").navigation();
+            }
+        }).setText("用ARouter跳转Login");
     }
 }
