f682810d8f74516634c713d79daeeaab37ced266
hsf <1152594956@qq.com>
2023-02-26 10:32:01 +0800

    打算使用ARouter传递json数据，但没成功

--- webview/src/main/java/com/hsf/webview/FirstActivity.java
+++ webview/src/main/java/com/hsf/webview/FirstActivity.java
@@ -3,16 +3,35 @@ package com.hsf.webview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
+import com.alibaba.android.arouter.facade.annotation.Autowired;
 import com.alibaba.android.arouter.facade.annotation.Route;
+import com.alibaba.android.arouter.facade.service.SerializationService;
+import com.alibaba.android.arouter.launcher.ARouter;
 import com.hsf.baselibs.LogActivity;
 
 @Route(path = "/url/FirstActivity")
 public class FirstActivity extends LogActivity {
 
+    @Autowired
+    String name;
+
+    @Autowired
+    int age;
+
+    @Autowired(name = "/service/json")
+    public ScoreBean scoreBean;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
+
+        ARouter.getInstance().inject(this);
+        SerializationService serializationService = ARouter.getInstance().navigation(SerializationService.class);
+        serializationService.init(this);
+
+        Log.d("Daisy", "打印传递的参数" + name + " " + age + " " + scoreBean);
     }
 }
